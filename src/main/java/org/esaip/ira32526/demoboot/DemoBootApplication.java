52941aeb63196430fba2645910662f480b5ba8d9
clemence6565217 <clemence.charles@outlook.be>
2026-03-27 11:46:39 +0100

    TP 2 commencé

--- src/main/java/org/esaip/ira32526/demoboot/DemoBootApplication.java
+++ src/main/java/org/esaip/ira32526/demoboot/DemoBootApplication.java
@@ -1,5 +1,7 @@
 package org.esaip.ira32526.demoboot;
 
+import org.esaip.ira32526.demoboot.service.BavardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +11,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoBootApplication {
 
+    // Injection de dépendance
+    @Autowired
+    private BavardService bavardService;
+
     public static void main(String[] args) {
         SpringApplication.run(DemoBootApplication.class, args);
     }
 
+    // route existante modifiée pour utiliser le service
     @GetMapping("hello")
     public String hello() {
-        return "Hello World !";
+        // Cela va s'afficher dans la console
+        System.out.println(bavardService.parler());
+        return "Regardez la console d'IntelliJ !";
+    }
+
+    // nouvelle route qui retourne directement le texte
+    @GetMapping("blabla")
+    public String blabla() {
+        return bavardService.parler();
     }
 }
